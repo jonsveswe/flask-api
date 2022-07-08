ed7691bd339274264e172e85743587de391f7c13
Jonas <jonsve999@gmail.com>
2022-07-08 16:05:15 +0200

    Added income GET and POST example. Added SenseHat and can get orientation from /orientation

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request
+from sense_hat import SenseHat
 
 app = Flask(__name__)
+sense = SenseHat()
+sense.set_imu_config(True, True, True)  # compass, gyroscope, accel
+
+incomes = [
+  { 'description': 'salary', 
+    'amount': 5000 }
+]
 
 @app.route('/')
 def index():
-    return 'Hello world'
-#     return jsonify({'name':'Jimit',
-#                     'address':'India'})
+    return 'Hej hej hemma'
+
+@app.route('/orientation', methods=['GET'])
+def get_orientation():
+    orientation = sense.get_orientation_degrees()
+    print(sense.orientation)
+    return jsonify(sense.orientation)
+
+@app.route('/incomes')# Default is GET
+def get_incomes():
+  return jsonify(incomes)
+  
+@app.route('/incomes', methods=['POST'])
+def add_income():
+  incomes.append(request.get_json())
+  print(incomes)
+  return '', 204        
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
